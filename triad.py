a4c2bcaa8df1dea1cb45e93bdc2d892f26d6f143
dotienduc113 <dotienduc113@gmail.com>
2024-11-11 17:24:15 +0700

    Update 11/11/24 Update code + fix bug

--- triad.py
+++ triad.py
@@ -34,7 +34,8 @@ def menu():
         if user_input.isdigit():
             choice = int(user_input)
             if choice == 1:
-                execute(choice)
+                run_query()
+                compare_checklist()
             elif choice == 2:
                 file_name = input("Input file name (blank for default):")
                 export_csv_table(file_name)
@@ -44,88 +45,6 @@ def menu():
                 print("Invalid input! Try again.")
         else:
             print("Invalid input! Try again.")
-
-
-'''
-# doan nay ko can
-# download velociraptor function
-def download_with_curl():
-    try:
-        curl_command = ("curl -L -o velociraptor.exe https://github.com/Velocidex/velociraptor/releases/download/v0"
-                        ".72/velociraptor-v0.72.0-windows-amd64.exe")
-        result = subprocess.run(curl_command, check=True)
-        print("Download successful using curl!")
-        return True
-    except FileNotFoundError:
-        print("Curl failed, trying with certutil...")
-        return False
-
-
-def download_with_certutil():
-    try:
-        certutil_cmd = (
-            'certutil.exe -urlcache -split -f https://github.com/Velocidex/velociraptor/releases/download/v0.72'
-            '/velociraptor-v0.72.0-windows-amd64.exe velociraptor.exe')
-        result = subprocess.run(certutil_cmd, check=True)
-        print("Download successful using certutil!")
-        return True
-    except FileNotFoundError:
-        # print("Certutil failed.")
-        return False
-
-
-def install_requirements():
-    if not download_with_curl():
-        if not download_with_certutil():
-            print("Both curl and certutil failed. Require manual install velociraptor.")
-    return
-'''
-
-'''
-def run_query1():
-    f = open("query.txt", "r")
-    count = 0
-    for line in f:
-        count = count + 1
-        cmd = '{0}'.format(line.strip())
-        # print(cmd)
-        try:
-            result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-            # print(result.stdout)  # Output the command result
-            print(result.stderr)  # Print any errors
-        except Exception as e:
-            print(f"Error running command: {e}")
-'''
-
-# dung de cho vao bang passed va failed
-'''
-def result_table1(passed, failed):
-    # Create the table with two columns
-    table = PrettyTable(max_table_width=100)
-    table.field_names = ["Passed", "Failed"]
-
-    # Determine the max length of passed/failed lists to balance the table rows
-    max_len = max(len(passed), len(failed))
-
-    # Add rows to the table, pairing passed and failed items
-    for i in range(max_len):
-        passed_item = passed[i] if i < len(passed) else ""
-        failed_item = failed[i] if i < len(failed) else ""
-        table.add_row([passed_item, failed_item])
-
-    # Print the table
-    print(table)
-    return
-'''
-
-
-def execute(choice):
-    #if choice == 0:
-    #    install_requirements()
-    if choice == 1:
-        run_query()
-        compare_checklist()
-        return
 
 
 if __name__ == "__main__":
